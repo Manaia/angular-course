2f1d415ebf2db805d10d139e1b9142c44aefe37a
Arthur Novais Viana Manaia <arthurm@maceng.mac.engenharia.com.br>
2018-10-09 09:05:34 -0300

    init project

---
+++ hello-typescript/main.js
@@ -0,0 +1,11 @@
+var variavel = 'variável';
+function funcao(x, y) {
+    return x + y;
+}
+var num = 2;
+var PI = 3.14;
+var numeros = [1, 2, 3];
+numeros.map(function (v) {
+    return v * 2;
+});
+numeros.map(function (v) { return v * 2; });
---
+++ hello-typescript/main.ts
@@ -0,0 +1,23 @@
+var variavel = 'variável';
+
+function funcao (x, y) {
+    return x + y;
+}
+
+let num = 2;
+const PI = 3.14;
+
+var numeros = [1, 2, 3];
+
+numeros.map(function(v){
+    return v * 2;
+});
+
+numeros.map(v => v * 2)
+
+class Matematica {
+    soma(x, y){
+        return x + y;
+    }
+}
+
